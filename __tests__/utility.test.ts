60a697a23cb22c3413f511c5fcff5785b487d380
Nataly Merezhuk <hello@nataly.dev>
2023-04-10 08:13:26 -0400

    Adds unit test.

---
+++ __tests__/utility.test.ts
@@ -0,0 +1,11 @@
+import { estimateNextSync } from "../utils/Utilities";
+
+describe("estimateNextSync()", () => {
+    test("estimateNextSync returns a Date object", () => {
+      const latest_sync = new Date("2023-04-01T06:00:29Z");
+      const average_sync = 2000000;
+      const nextSync = estimateNextSync(latest_sync, average_sync);
+
+      expect(nextSync).toBeInstanceOf(Date);
+    });
+})
